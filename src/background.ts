56445ae66416e43cb4309cc6507f2fed1cbc5899
Сенько Александр <aleksandr.senko@gazprombank.ru>
2025-04-12 13:06:30 +0300

    refactor: better alarms

--- src/background.ts
+++ src/background.ts
@@ -2,70 +2,92 @@ import dayjs from 'dayjs';
 
 import iconUrl from './assets/icon.png';
 import { ACTION } from './types';
-import { getRecordsFromStorage } from './lib';
+import { formatTime, getRecordsFromStorage, hasKeys } from './lib';
+
+const REST_ALARM = 'rest-alarm';
+const WORK_ALARM = 'work-alarm';
 
 chrome.alarms.onAlarm.addListener(async ({ name }) => {
   console.log('onAlarm', name);
-  const {
-    notify = false,
-    workTime = 45,
-    restTime = 15,
-  } = await chrome.storage.sync.get({
-    notify: false,
-    workTime: 45,
-    restTime: 15,
-  });
 
-  if (!notify) return;
-
-  const records = await getRecordsFromStorage({ sort: 'desc' });
-  if (records[0]?.action === ACTION.LOGIN) {
-    const passed = dayjs(Date.now()).diff(records[0].time, 'minute');
-    const diff = passed % (workTime + restTime);
-    console.log('passed', passed, 'diff', diff);
-    if (passed > 0 && (diff === workTime || diff === 0)) {
-      const message = diff === 0 ? "Let's keep working!" : "Let's take a break!";
-      console.log('notify', message);
-      chrome.notifications.create(
-        '',
-        {
-          type: 'basic',
-          title: 'Work Timer',
-          message,
-          iconUrl,
-          requireInteraction: true,
-        },
-        function () {
-          // empty
-        },
-      );
-    }
+  if (name !== REST_ALARM && name !== WORK_ALARM) {
+    return;
   }
+
+  const message = name === WORK_ALARM ? "Let's keep working!" : "Let's take a break!";
+  console.log('notify', message);
+  chrome.notifications.create(
+    '',
+    {
+      type: 'basic',
+      title: 'Work Timer',
+      message,
+      iconUrl,
+      requireInteraction: true,
+    },
+    function () {
+      // empty
+    },
+  );
 });
 
-chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+chrome.runtime.onInstalled.addListener(({ reason }) => {
   console.log('onInstalled', reason);
 
-  await createReminder();
+  chrome.storage.onChanged.addListener(onStorageChanged);
+
+  checkAlarms();
+  updateBadgeText();
 });
 
 chrome.runtime.onStartup.addListener(async () => {
   console.log('onStartup');
 
-  const records = await getRecordsFromStorage({ sort: 'desc' });
-  const text = records[0]?.action === ACTION.LOGIN ? 'in' : 'out';
-  chrome.action.setBadgeText({ text });
+  if (!chrome.storage.onChanged.hasListener(onStorageChanged)) {
+    chrome.storage.onChanged.addListener(onStorageChanged);
+  }
 
-  await createReminder();
+  checkAlarms();
+  updateBadgeText();
 });
 
-async function createReminder() {
-  const reminder = await chrome.alarms.get('remider');
-  if (!reminder) {
-    console.log('create reminder');
-    chrome.alarms.create('remider', {
-      delayInMinutes: 1,
-      periodInMinutes: 1,
-    });
+function onStorageChanged(changes: Record<string, chrome.storage.StorageChange>, areaName: chrome.storage.AreaName) {
+  if (
+    (areaName === 'sync' && hasKeys(changes, 'notify', 'workTime', 'restTime')) ||
+    (areaName === 'local' && hasKeys(changes, 'records'))
+  ) {
+    checkAlarms();
   }
+}
+
+function checkAlarms() {
+  (async () => {
+    const { notify, workTime, restTime } = await chrome.storage.sync.get({ notify: false, workTime: 45, restTime: 15 });
+    const records = await getRecordsFromStorage({ sort: 'desc' });
+    const lastLogin = (records[0]?.action === ACTION.LOGIN && records[0].time) || undefined;
+
+    if (!notify || !lastLogin) {
+      console.log('clear all alarms');
+      chrome.alarms.clearAll();
+    } else {
+      const periodInMinutes = workTime + restTime;
+      const minutesSinceLastLogin = dayjs(Date.now()).diff(lastLogin, 'minute');
+
+      let delayInMinutes = workTime - (minutesSinceLastLogin % periodInMinutes);
+      console.log('create alarm', REST_ALARM, { delayInMinutes, periodInMinutes, lastLogin: formatTime(lastLogin) });
+      chrome.alarms.create(REST_ALARM, { delayInMinutes, periodInMinutes });
+
+      delayInMinutes += restTime;
+      console.log('create alarm', WORK_ALARM, { delayInMinutes, periodInMinutes, lastLogin: formatTime(lastLogin) });
+      chrome.alarms.create(WORK_ALARM, { delayInMinutes, periodInMinutes });
+    }
+  })();
+}
+
+function updateBadgeText() {
+  (async () => {
+    const records = await getRecordsFromStorage({ sort: 'desc' });
+    const text = records[0]?.action === ACTION.LOGIN ? 'in' : 'out';
+    chrome.action.setBadgeText({ text });
+  })();
 }
